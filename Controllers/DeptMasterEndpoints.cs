2c6c67c19ccd68f314b774518fa1a50d28429f89
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-13 15:39:03 +0530

    Controllers

---
+++ Controllers/DeptMasterEndpoints.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmsFullStackApplication.Data;
+using EmsFullStackApplication.Models;
+namespace EmsFullStackApplication.Controllers;
+
+public static class DeptMasterEndpoints
+{
+    public static void MapDeptMasterEndpoints (this IEndpointRouteBuilder routes)
+    {
+        routes.MapGet("/api/DeptMaster", async (DeptMasterContext db) =>
+        {
+            return await db.DeptMaster.ToListAsync();
+        })
+        .WithName("GetAllDeptMasters")
+        .Produces<List<DeptMaster>>(StatusCodes.Status200OK);
+
+        routes.MapGet("/api/DeptMaster/{id}", async (int DeptCode, DeptMasterContext db) =>
+        {
+            return await db.DeptMaster.FindAsync(DeptCode)
+                is DeptMaster model
+                    ? Results.Ok(model)
+                    : Results.NotFound();
+        })
+        .WithName("GetDeptMasterById")
+        .Produces<DeptMaster>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+
+        routes.MapPut("/api/DeptMaster/{id}", async (int DeptCode, DeptMaster deptMaster, DeptMasterContext db) =>
+        {
+            var foundModel = await db.DeptMaster.FindAsync(DeptCode);
+
+            if (foundModel is null)
+            {
+                return Results.NotFound();
+            }
+
+            db.Update(deptMaster);
+
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithName("UpdateDeptMaster")
+        .Produces(StatusCodes.Status404NotFound)
+        .Produces(StatusCodes.Status204NoContent);
+
+        routes.MapPost("/api/DeptMaster/", async (DeptMaster deptMaster, DeptMasterContext db) =>
+        {
+            db.DeptMaster.Add(deptMaster);
+            await db.SaveChangesAsync();
+            return Results.Created($"/DeptMasters/{deptMaster.DeptCode}", deptMaster);
+        })
+        .WithName("CreateDeptMaster")
+        .Produces<DeptMaster>(StatusCodes.Status201Created);
+
+        routes.MapDelete("/api/DeptMaster/{id}", async (int DeptCode, DeptMasterContext db) =>
+        {
+            if (await db.DeptMaster.FindAsync(DeptCode) is DeptMaster deptMaster)
+            {
+                db.DeptMaster.Remove(deptMaster);
+                await db.SaveChangesAsync();
+                return Results.Ok(deptMaster);
+            }
+
+            return Results.NotFound();
+        })
+        .WithName("DeleteDeptMaster")
+        .Produces<DeptMaster>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+    }
+	public static void MapDeptMasterEndpoints (this IEndpointRouteBuilder routes)
+    {
+        routes.MapGet("/api/DeptMaster", async (fullprofileDbContext db) =>
+        {
+            return await db.DeptMaster.ToListAsync();
+        })
+        .WithName("GetAllDeptMasters")
+        .Produces<List<DeptMaster>>(StatusCodes.Status200OK);
+
+        routes.MapGet("/api/DeptMaster/{id}", async (int EmpCode, fullprofileDbContext db) =>
+        {
+            return await db.DeptMaster.FindAsync(EmpCode)
+                is DeptMaster model
+                    ? Results.Ok(model)
+                    : Results.NotFound();
+        })
+        .WithName("GetDeptMasterById")
+        .Produces<DeptMaster>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+
+        routes.MapPut("/api/DeptMaster/{id}", async (int EmpCode, DeptMaster deptMaster, fullprofileDbContext db) =>
+        {
+            var foundModel = await db.DeptMaster.FindAsync(EmpCode);
+
+            if (foundModel is null)
+            {
+                return Results.NotFound();
+            }
+
+            db.Update(deptMaster);
+
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithName("UpdateDeptMaster")
+        .Produces(StatusCodes.Status404NotFound)
+        .Produces(StatusCodes.Status204NoContent);
+
+        routes.MapPost("/api/DeptMaster/", async (DeptMaster deptMaster, fullprofileDbContext db) =>
+        {
+            db.DeptMaster.Add(deptMaster);
+            await db.SaveChangesAsync();
+            return Results.Created($"/DeptMasters/{deptMaster.EmpCode}", deptMaster);
+        })
+        .WithName("CreateDeptMaster")
+        .Produces<DeptMaster>(StatusCodes.Status201Created);
+
+
+        routes.MapDelete("/api/DeptMaster/{id}", async (int EmpCode, fullprofileDbContext db) =>
+        {
+            if (await db.DeptMaster.FindAsync(EmpCode) is DeptMaster deptMaster)
+            {
+                db.DeptMaster.Remove(deptMaster);
+                await db.SaveChangesAsync();
+                return Results.Ok(deptMaster);
+            }
+
+            return Results.NotFound();
+        })
+        .WithName("DeleteDeptMaster")
+        .Produces<DeptMaster>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+    }
+}
---
+++ Controllers/EmployeeEndpoints.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmsFullStackApplication.Data;
+using EmsFullStackApplication.Models;
+namespace EmsFullStackApplication.Controllers;
+
+public static class EmployeeEndpoints
+{
+    public static void MapEmployeeEndpoints (this IEndpointRouteBuilder routes)
+    {
+        routes.MapGet("/api/Employee", async (EmployeeDbContext db) =>
+        {
+            return await db.Employee.ToListAsync();
+        })
+        .WithName("GetAllEmployees")
+        .Produces<List<Employee>>(StatusCodes.Status200OK);
+
+        routes.MapGet("/api/Employee/{id}", async (int EmpCode, EmployeeDbContext db) =>
+        {
+            return await db.Employee.FindAsync(EmpCode)
+                is Employee model
+                    ? Results.Ok(model)
+                    : Results.NotFound();
+        })
+        .WithName("GetEmployeeById")
+        .Produces<Employee>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+
+        routes.MapPut("/api/Employee/{id}", async (int EmpCode, Employee employee, EmployeeDbContext db) =>
+        {
+            var foundModel = await db.Employee.FindAsync(EmpCode);
+
+            if (foundModel is null)
+            {
+                return Results.NotFound();
+            }
+
+            db.Update(employee);
+
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithName("UpdateEmployee")
+        .Produces(StatusCodes.Status404NotFound)
+        .Produces(StatusCodes.Status204NoContent);
+
+        routes.MapPost("/api/Employee/", async (Employee employee, EmployeeDbContext db) =>
+        {
+            db.Employee.Add(employee);
+            await db.SaveChangesAsync();
+            return Results.Created($"/Employees/{employee.EmpCode}", employee);
+        })
+        .WithName("CreateEmployee")
+        .Produces<Employee>(StatusCodes.Status201Created);
+
+        routes.MapDelete("/api/Employee/{id}", async (int EmpCode, EmployeeDbContext db) =>
+        {
+            if (await db.Employee.FindAsync(EmpCode) is Employee employee)
+            {
+                db.Employee.Remove(employee);
+                await db.SaveChangesAsync();
+                return Results.Ok(employee);
+            }
+
+            return Results.NotFound();
+        })
+        .WithName("DeleteEmployee")
+        .Produces<Employee>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+    }
+}
