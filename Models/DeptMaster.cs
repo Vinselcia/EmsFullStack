f79162ae735356266d6483590574342f4c0d52b4
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-13 15:41:04 +0530

    Models

---
+++ Models/DeptMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EmsFullStackApplication.Models
+{
+    public class DeptMaster
+    {
+        [Key]
+       public int EmpCode {  get; set; }
+        public string ? EmpName {  get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string ? Email { get; set; }
+        public int DeptCode {  get; set; }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace EmsFullStackApplication.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EmpCode { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? EmpName { get; set; }
+        public string? Email { get; set; }
+        public int DeptCode { get; set; }
+      
+    }
+}
